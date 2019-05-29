d3b0e73b48befc1f14914361309cdf63ff230f98
IVIuho <eogh2811@naver.com>
2019-05-29 15:12:25 +0900

    edit test code

--- test.py
+++ test.py
@@ -15,23 +15,27 @@ req_game_event = test_data_set["game_event"]
 req_check = test_data_set["check_answer"]
 
 
-def get_login():
-    return json.dumps(req_login[int(input("User ID index: "))]).encode()
+def get_login(index):
+    return json.dumps(req_login[index]).encode()
 
 
 def make_room():
     return json.dumps(req_make_room).encode()
 
 
-def join_room():
+def join_room(room_id):
     req = req_join_room
-    req["Data"]["roomId"] += input("Room ID: ")
+    req["Data"]["roomId"] += room_id
 
     return json.dumps(req).encode()
 
 
-def get_event():
-    return json.dumps(req_game_event).encode()
+def get_event(user_id, room_id):
+    req = req_game_event
+    req["Data"]["userId"] = user_id
+    req["Data"]["roomId"] += room_id
+
+    return json.dumps(req).encode()
 
 
 def check_answer():
@@ -42,7 +46,7 @@ def check_answer():
 
 
 def read(sock, buffer=1024):
-    msg = sock.recv(buffer)
+    msg = sock.recv(buffer).decode()
     print("> {0}".format(msg))
 
 
@@ -50,21 +54,24 @@ def run():
     with socket.socket() as sock:
         sock.connect(("localhost", 12346))
 
-        sock.send(get_login())
+        index = int(input("User ID index: "))
+
+        sock.send(get_login(index))
         read(sock)
 
-        yes_or_no = input("Do you want to make a room? [Y / N]").upper()
+        yes_or_no = input("Do you want to make a room? [Y / N] >> ").upper()
         if yes_or_no == "Y":
             sock.send(make_room())
             read(sock)
-
-        sock.send(join_room())
-        read(sock)
+        else:
+            room_id = input("Room ID: ")
+            sock.send(join_room(room_id))
+            read(sock)
 
         input("Input enter to start game.")
         for i in range(3):
             print("attempts: {0}".format(i))
-            sock.send(get_event())
+            sock.send(get_event(req_login[index]["Data"]["userId"], room_id))
             read(sock)
             time.sleep(1)
 
